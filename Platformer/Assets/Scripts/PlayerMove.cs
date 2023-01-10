749f3e32db8c3dd9b70de034ef289c724675b5f8
Hunter-James <93853751+Hunter-James@users.noreply.github.com>
2023-01-11 08:46:28 +1000

    Game Platformer

--- Platformer/Assets/Scripts/PlayerMove.cs
+++ Platformer/Assets/Scripts/PlayerMove.cs
@@ -1,17 +1,32 @@
 using UnityEngine;
 using System.Collections;
+using UnityEditor.Tilemaps;
 
 public class PlayerMove : MonoBehaviour
 {
     public float speed;
-    public float force;
-    private Vector2 direction;
-    private Rigidbody2D rigidbody;
+    public float moveInput;
+    private Rigidbody2D ridbody;
+    public SpriteRenderer spriteRen;
 
+    private void Start()
+    {
+        ridbody = GetComponent<Rigidbody2D>();
 
+    }
     void FixedUpdate()
     {
-        
-        
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        moveInput = Input.GetAxis("Horizontal");
+        ridbody.velocity = new Vector2(moveInput * speed, ridbody.velocity.y);
+        if (moveInput < 0)
+        {
+            spriteRen.flipX = true;
+        }
+        if (moveInput > 0)
+        {
+            spriteRen.flipX = false;
+        }
+
     }
 }
