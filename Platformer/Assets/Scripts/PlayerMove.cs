25d1950b131eec7bb0a1ae31a392221ab24aa801
Hunter-James <93853751+Hunter-James@users.noreply.github.com>
2023-01-11 14:46:11 +1000

    Update PlayerMove.cs

--- Platformer/Assets/Scripts/PlayerMove.cs
+++ Platformer/Assets/Scripts/PlayerMove.cs
@@ -4,9 +4,10 @@ using UnityEditor.Tilemaps;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float force;
     public float speed;
     public float moveInput;
-    private Rigidbody2D ridbody;
+    public Rigidbody2D ridbody;
     public SpriteRenderer spriteRen;
 
     private void Start()
@@ -14,11 +15,25 @@ public class PlayerMove : MonoBehaviour
         ridbody = GetComponent<Rigidbody2D>();
 
     }
-    void FixedUpdate()
+
+    private void Update()
     {
+        //Move Character X
         GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
         moveInput = Input.GetAxis("Horizontal");
         ridbody.velocity = new Vector2(moveInput * speed, ridbody.velocity.y);
+
+        //Jump Character Y
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ridbody.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        //Flip Character
         if (moveInput < 0)
         {
             spriteRen.flipX = true;
